444785b9b524465706919bcb3d85c5fea5afd741
brandon <bharman632@gmail.com>
2022-07-05 17:15:22 -0700

    Add project files.

---
+++ Circle Objects/Circle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    internal class Circle
+    {
+
+        public double radius;
+        public double circumference;
+        public double area;
+
+        //They watch other methods return their values
+        double cuckArea = 0;
+        double cuckCircumference = 0;
+
+
+        public Circle(double userRadius)
+        {
+            radius = userRadius;
+        }
+        
+        //public string CalculateFormattedCircumference()
+        //public string CalculateFormattedArea()
+        //private string FormatNumber(double x)
+       
+        public double CalculateCircumference()
+        {
+            return double.Parse(CalculateFormattedCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            return double.Parse(CalculateFormattedArea());
+            
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            cuckCircumference = 2 * radius * Math.PI;
+            return Math.Round(cuckCircumference, 2).ToString(); 
+
+        }
+
+        public string CalculateFormattedArea()
+        {
+            cuckArea = radius* radius *Math.PI;
+            return Math.Round(cuckArea, 2).ToString();
+        }
+
+
+        //I dont know what this does as it doesn't seem to matter as I put the formula in the other methods?
+        private string FormatNumber(double x)
+        {
+            return "1";
+        }
+
+        
+
+
+
+
+    }
+}
+
+
+
+
+
+/*public Circle(double radius)
+public double CalculateCircumference()
+public string CalculateFormattedCircumference()
+public double CalculateArea()
+public string CalculateFormattedArea()
+private string FormatNumber(double x)
+*/
---
+++ Circle Objects/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+using Circle_Objects;
+double userRadius = 0;
+string input2 = "";
+bool loop = true;
+bool loop2 = true;
+int z = 0;
+Circle myCircle = new Circle(0);
+
+do
+{
+    do
+    {
+        Console.WriteLine("Please enter a radius number...");
+        string input = (Console.ReadLine());
+
+
+        if (double.TryParse(input, out userRadius))
+        {
+
+            z++;
+            myCircle.radius = userRadius;
+            
+            loop = false;
+            Console.WriteLine($"The circumference is {myCircle.CalculateCircumference()}");
+            Console.WriteLine($"The area is {myCircle.CalculateArea()}");
+
+            Console.WriteLine();
+            Console.WriteLine("Want to enter another radius? Y or N");
+            input2 = Console.ReadLine();
+
+            if (input2.ToLower() == "y")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("uhhh bye...");
+                Console.WriteLine($"Circles built {z}");
+                loop2 = false;
+            }
+        }
+        else
+        {
+
+            Console.WriteLine("Error not a number!");
+            Console.WriteLine();
+        }
+
+
+
+    }
+    while (loop == true);
+
+
+
+    
+}
+while (loop2 == true);
